5c68d9d85e16b1b929172716daf8bc6219971e62
OmarMWarraich <earthling986@gmail.com>
2024-04-09 01:37:06 +0500

    fix: Authorized_Keys

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,26 @@
-import type { Metadata } from 'next'
-import { Lexend } from 'next/font/google'
+import type { Metadata } from "next";
+import { Lexend } from "next/font/google";
 
-import './globals.css'
-import { cn } from '@/lib/utils'
-import Navbar from '@/components/Navbar'
-import { Provider } from '@/components/Providers'
-import { Toaster } from '@/components/ui/toaster'
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
+import { Provider } from "@/components/Providers";
+import { Toaster } from "@/components/ui/toaster";
 
-const lexend = Lexend({ subsets: ['latin'] })
+const lexend = Lexend({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Learning Journey',
-}
+  title: "AI Course Generator",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={cn(
-        lexend.className, 'antialiased min-h-screen pt-16'
-      )}>
+      <body className={cn(lexend.className, "antialiased min-h-screen pt-16")}>
         <Provider>
           <Navbar />
           {children}
@@ -30,5 +28,5 @@ export default function RootLayout({
         </Provider>
       </body>
     </html>
-  )
+  );
 }
